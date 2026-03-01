0335fc0ce6fd532ec951b58c5920eeb340153464
richcto <rjones2102@gmail.com>
2026-03-01 11:38:17 +0000

    Add S3 backend

--- producers/lambdas/provider.tf
+++ producers/lambdas/provider.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "s3" {
+    bucket = "terraform-state-237617081322"
+    key    = "ba-playground/producers-lambdas/terraform.tfstate"
+    region = "eu-west-2"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
